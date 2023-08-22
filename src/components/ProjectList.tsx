c6cfc69f2f35b97e6fa5f79c18d5ed5238dcc42b
meee-low <my_milo_m@hotmail.com>
2023-08-21 23:18:49 -0300

    hid highlighted projects from the "other projects" section

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -6,13 +6,19 @@ import projectsData from "../data/projects.json";
 
 // Use the data in your component
 const ProjectList: React.FC = () => {
-  const projects: ProjectData[] = projectsData;
+  // const projects: ProjectData[] = projectsData;
+
+  // Highlighted projects already go to the highlighted area.
+  // Don't put them there again.
+  const nonHighlightedProjects: ProjectData[] = projectsData.filter(
+    (project) => !project.highlighted
+  );
 
   return (
     <section className="project-list">
       <h2>Other Projects</h2>
       <div className="project-grid">
-        {projects.map((project) => (
+        {nonHighlightedProjects.map((project) => (
           <ProjectListCard key={project.id} project={project} />
         ))}
       </div>
