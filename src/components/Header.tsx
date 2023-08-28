3ba5c9a8688064a722f7aee36635ea554899733c
meee-low <my_milo_m@hotmail.com>
2023-08-28 20:47:00 -0300

    Removed unnecessary console.log-ing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,6 @@ import { Link, useLocation } from "react-router-dom";
 
 const Header: React.FC = () => {
   const location = useLocation();
-  console.log(location);
 
   const navBar: Record<string, string> = {
     "/": "Projects",
