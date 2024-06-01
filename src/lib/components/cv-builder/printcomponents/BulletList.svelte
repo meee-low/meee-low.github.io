93a4b4e03f08f72af6c5da709b09eea9190fc922
M. F. Drumond <mat_drumond@hotmail.com>
2024-06-01 11:51:42 -0300

    Bullet Lists now accept markdown

--- src/lib/components/cv-builder/printcomponents/BulletList.svelte
+++ src/lib/components/cv-builder/printcomponents/BulletList.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
   export let bullets: string[];
   export let gridCols: number = 1;
+
+  import {formatBasicMarkdown} from "$lib/markdownParsing"
 </script>
 
 {#if bullets && bullets.length > 0}
-  <ul class="list-disc pl-8 grid print:leading-snug tracking-tight" style="grid-template-columns: repeat({gridCols}, minmax(0, 1fr));">
+  <ul
+    class="grid list-disc pl-8 tracking-tight print:leading-snug"
+    style="grid-template-columns: repeat({gridCols}, minmax(0, 1fr));"
+  >
     {#each bullets as bullet}
       {#if bullet}
-      <li class="">{bullet}</li>
+        <li class="">{@html formatBasicMarkdown(bullet)}</li>
       {/if}
     {/each}
   </ul>
---
+++ src/lib/markdownParsing.ts
@@ -0,0 +1,37 @@
+interface MarkdownStyling {
+  bold: string;
+  italic: string;
+}
+
+export function makeMarkdownFormatter(
+  styling: MarkdownStyling,
+): (s: string) => string {
+  return (s: string) =>
+    sanitize(s)
+      .replace("\\*", "&#42")
+      .replace(
+        /(?<!\\)\*\*(.*?)(?<!\\)\*\*/g,
+        `<strong class="${styling.bold}">$1</strong>`,
+      )
+      .replace(
+        /(?<!\\)\*(.*?)(?<!\\)\*/g,
+        `<em class="${styling.italic}">$1</em>`,
+      );
+}
+
+function sanitize(input: string): string {
+  return input
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#x27;");
+}
+
+export function formatBasicMarkdown(input: string): string {
+  const basicFormatter = makeMarkdownFormatter({
+    bold: "font-semibold",
+    italic: "italic",
+  });
+  return basicFormatter(input);
+}
