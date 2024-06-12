ea35678e8c196a064d59093ed5ba992a0ddb45c9
M. F. Drumond <mat_drumond@hotmail.com>
2024-06-12 18:15:14 -0300

    Pushed the icon inside the selection

--- src/lib/components/LanguageSelector.svelte
+++ src/lib/components/LanguageSelector.svelte
@@ -2,12 +2,19 @@
   let languageSelected: string;
 </script>
 
-<label>
-  <i class="fa-solid fa-globe">
-  <span class="sr-only">Language</span>
-</i>
-<select bind:value={languageSelected} class="rounded-full text-black p-1">
-  <option value="en" class="text-black" selected>English</option>
-  <option value="pt-br" class="text-black">Português</option>
-</select>
-</label>
+<div class="relative">
+  <label>
+    <i
+      class="fa-solid fa-globe fa-fw absolute left-1 top-1/2 -translate-y-1/2 text-black"
+    >
+      <span class="sr-only">Language</span>
+    </i>
+    <select
+      bind:value={languageSelected}
+      class="rounded-full p-1 pl-5 text-black"
+    >
+      <option value="en" class="text-black" selected>English</option>
+      <option value="pt-br" class="text-black">Português</option>
+    </select>
+  </label>
+</div>
