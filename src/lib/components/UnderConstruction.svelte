f456ad75fa6f917a16507128d5e31c57a2bd6e2f
M. F. Drumond <30362944+meee-low@users.noreply.github.com>
2024-08-21 11:58:36 -0300

    Made the under construction sign more centered.
    Still not fully happy with it

--- src/lib/components/UnderConstruction.svelte
+++ src/lib/components/UnderConstruction.svelte
@@ -1,3 +1,3 @@
-<div class="mt-12 flex h-full items-center justify-center">
+<div class="min-h-[80vh] flex items-center justify-center">
   <h1 class=" text-5xl font-bold">Under Construction...</h1>
 </div>
