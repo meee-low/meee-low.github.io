1209133bbf77e9d8952d07308fc9dd9886de52c2
Matheus Ferreira Drumond <30362944+meee-low@users.noreply.github.com>
2024-09-10 07:26:28 -0300

    amend(ranker): add the winrate strategy

---
+++ src/routes/interactive/bracketeering/ranker/winrate_ranker.ts
@@ -0,0 +1,12 @@
+import type { Matrix } from "$lib/math/linalg";
+
+export function rankByWinrate(m: Matrix): number[] {
+  let result = [];
+
+  for (let i = 0; i < m.rows; ++i) {
+    const wins: number = m.getRow(i).reduce((acc, x) => acc + x, 0) + 1;
+    const losses: number = m.getCol(i).reduce((acc, x) => acc + x, 0) + 1;
+    result.push(wins / (wins + losses));
+  }
+  return result;
+}
